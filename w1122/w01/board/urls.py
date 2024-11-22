48c74780f7e2ee2dd88ae43a0d749f019558d7f3
joung <wonwow55@naver.com>
2024-11-22 18:17:46 +0900

    11/22

---
+++ w1122/w01/board/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path,include
+from . import views
+
+app_name='board'
+urlpatterns = [
+    path('breply/<int:bno>/', views.breply ,name='breply'),  # 답변달기
+    path('bdelete/<int:bno>/', views.bdelete ,name='bdelete'),  # 글삭제
+    path('bmodify/<int:bno>/', views.bmodify ,name='bmodify'),  # 글상세보기
+    path('bview/<int:bno>/', views.bview ,name='bview'),  # 글상세보기
+    path('bwrite/', views.bwrite ,name='bwrite'),        # 글쓰기
+    path('blist/', views.blist ,name='blist'),           # 게시판리스트
+]
