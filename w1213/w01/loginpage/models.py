66cb6f231fd7bfd9c4f8fd0f5f73a6ec3badf42f
vhfflxbzmffpdhxkdlem <wonwow55@naver.com>
2024-12-17 09:30:11 +0900

    1234

---
+++ w1213/w01/loginpage/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# import datetime
+from datetime import datetime
+class Member(models.Model):
+  id = models.CharField(max_length=50,primary_key=True)
+  pw = models.CharField(max_length=100)
+  name = models.CharField(max_length=100)
+  nicName = models.CharField(max_length=100)
+  mail = models.EmailField(max_length=100)
+  birthday = models.CharField(max_length=50)
+  gender = models.CharField(max_length=10,choices=[('남자', '남자'), ('여자', '여자')])
+  mdate = models.DateTimeField(auto_now=True)
+  # mdate = models.DateTimeField(default=datetime.now())
+  # 테스트
+
+
+  def __str__(self):
+    return self.name
